a2c0727512ae0690947f0c6bc78065156a83f451
Matthew Wen <mattwen2018@gmail.com>
2022-01-13 20:06:59 -0500

    Initial Setup from Template (#1)
    * Template from Purdue ECESS and ECEA
    * Template was made entirely by Matthew Wen and James Donnelly

---
+++ scripts/scriptMain.ts
@@ -0,0 +1,10 @@
+import {MyFbStorage} from "../src/google/myFb/myFbStorage";
+import {MyFbAuth} from "../src/google/myFb/myFbAuth";
+
+// My hack to keep the process alive:
+const main = async () => {
+}
+
+main().then(() => {
+    console.log("Main")
+});
---
+++ src/utils/api.ts
@@ -0,0 +1,40 @@
+import expressWs, {Application} from "express-ws";
+import express from "express";
+
+export class Api {
+    static default: Api = new Api();
+
+    ws: expressWs.Instance;
+
+    private constructor() {
+        this.ws = expressWs(express());
+    }
+
+    get app(): Application {
+        return this.ws.app;
+    }
+
+    static setUse(func: any) {
+        this.default.app.use(func);
+    }
+
+    static setVoidApp(func: any) {
+        this.default.app.use(func);
+    }
+
+    static setGetRoute(route: string, func: any) {
+        this.default.app.get(route, func);
+    }
+
+    static setPostRoute(route: string, func: any) {
+        this.default.app.post(route, func);
+    }
+
+    static setWs(route: string, func: any) {
+        this.default.app.ws(route, func);
+    }
+
+    static listen() {
+        this.default.app.listen(process.env.PORT || 8000);
+    }
+}
