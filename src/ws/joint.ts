e098bdfb40f0f56e80d9def714f9c027e974bdc8
Matthew Wen <mattwen2018@gmail.com>
2022-01-19 13:10:39 -0500

    finished debugging, change joint to joints (#4)

--- src/ws/joint.ts
+++ src/ws/joint.ts
@@ -1,14 +1,12 @@
 import {Request, Response} from "express";
-import expressWs, {WebsocketRequestHandler} from "express-ws";
 import {decodeIDToken} from "../utils/auth";
 import * as ws from 'ws';
 import {MyFbRTDb} from "../google/myFb/myFbRTDb";
 
 
-export const jointWs = async (ws: ws, req: Request, res: Response) => {
-    const response = await decodeIDToken(req);
+export const jointsWs = async (ws: ws, req: Request, res: Response) => {
     const id = req.params.id;
-    if (response) {
+    if (await decodeIDToken(req)) {
         ws.on('message', async function(msg: string) {
             try {
                 const json = JSON.parse(msg);
@@ -21,6 +19,5 @@ export const jointWs = async (ws: ws, req: Request, res: Response) => {
     }
     else {
         ws.close();
-        res.sendStatus(400);
     }
 }
